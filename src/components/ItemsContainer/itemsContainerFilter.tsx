db580d6e1da8578951ee6bfda26d988ea54a5a9d
neet-007 <moayedeffect@gmail.com>
2024-08-28 15:36:59 +0300

    basic filtring from fake api and client

--- src/components/ItemsContainer/itemsContainerFilter.tsx
+++ src/components/ItemsContainer/itemsContainerFilter.tsx
@@ -1,6 +1,7 @@
-import React, { ComponentProps } from "react";
+import React, { ComponentProps, useRef } from "react";
 import "./itemsContainer.css"
 import { Button } from "../Shared/Button";
+import { useLocation, useNavigate, NavigateFn } from "@tanstack/react-router";
 
 type ItemsContainerFilterProps = {
 	isFilterOpen: boolean;
@@ -9,32 +10,49 @@ type ItemsContainerFilterProps = {
 }
 
 type CheckBoxProps = {
-	item: string
+	item: string;
+	filters: string;
+	navigate: NavigateFn
+	containerName: string
 }
 
 type CheckBoxContainerProps = {
 	containerName: string;
 	items: string[]
+	filters: string;
+	navigate: NavigateFn
 }
 
 type ButtonsContainer = {
 	containerName: string;
-	buttons: string[]
+	buttons: string[];
+	filters: string;
+	navigate: NavigateFn
 }
 
 const CheckBox: React.FC<ComponentProps<"div"> & CheckBoxProps> = ({
-	item, ...props }) => {
+	item, navigate, filters, containerName, ...props }) => {
+	const loaction = useLocation();
 	return (
 		<div {...props}>
 			<input id={`check-box-${item}-${item}`}
-				type="checkbox" value={item} />
+				type="checkbox" value={item}
+				onChange={(e) => {
+					navigate({
+						search: {
+							...loaction.search,
+							[containerName.toLocaleLowerCase()]: e.target.value.toLocaleLowerCase()
+						}
+					})
+				}}
+			/>
 			<label htmlFor="">{item}</label>
 		</div>
 	)
 }
 
 const CheckBoxContainer: React.FC<ComponentProps<"div"> & CheckBoxContainerProps> = ({
-	containerName, items, ...props }) => {
+	containerName, items, navigate, filters, ...props }) => {
 	return (
 		<div className="items-container-filter-checkbox" {...props}>
 			<div>
@@ -43,7 +61,11 @@ const CheckBoxContainer: React.FC<ComponentProps<"div"> & CheckBoxContainerProps
 			<div>
 				{items.map((v, i) => (
 					<CheckBox key={`checkbox-item${v}-${i}`}
-						item={v} />
+						item={v}
+						navigate={navigate}
+						filters={filters}
+						containerName={containerName}
+					/>
 				))}
 			</div>
 		</div>
@@ -52,7 +74,7 @@ const CheckBoxContainer: React.FC<ComponentProps<"div"> & CheckBoxContainerProps
 }
 
 const ButtonContainer: React.FC<ComponentProps<"div"> & ButtonsContainer> = ({
-	containerName, buttons, ...props }) => {
+	containerName, buttons, navigate, filters, ...props }) => {
 	return (
 		<div {...props}>
 			<div>
@@ -73,8 +95,11 @@ const ButtonContainer: React.FC<ComponentProps<"div"> & ButtonsContainer> = ({
 
 export const ItemsContainerFilter: React.FC<ComponentProps<"div"> & ItemsContainerFilterProps> = (
 	{ isFilterOpen, setIsFilterOpen, filtersOptions, ...props }) => {
+	const location = useLocation();
+	const navigate = useNavigate();
 
-	console.log(filtersOptions)
+	const filters = location.search.filter || ""
+	console.log(filters)
 	return (
 		<div className={`items-container-filter ${isFilterOpen ? "items-container-filter-open" : ""}`} {...props}>
 			<div className="items-container-filter-header">
@@ -86,12 +111,19 @@ export const ItemsContainerFilter: React.FC<ComponentProps<"div"> & ItemsContain
 			</div>
 			{[...filtersOptions].map(([key, value]) => {
 				return <CheckBoxContainer containerName={key}
-					items={value} />
+					items={value}
+					filters={filters}
+					navigate={navigate}
+				/>
 			})}
 			<ButtonContainer containerName="sizes"
 				buttons={["35", "43", "44",
 					"14", "41", "53"
-				]} />
+				]}
+
+				filters={filters}
+				navigate={navigate}
+			/>
 			<div className="items-container-filter-slider">
 				slider
 			</div>
--- src/helperFunctions/fetchProducts.ts
+++ src/helperFunctions/fetchProducts.ts
@@ -1,59 +1,78 @@
 import { ItemType } from "../components/ItemCard/ItemCard";
 
 type ReturnType = {
-  results: ItemType[];
-  page: number;
-  carousel: ItemType[];
-  filters: Map<string, string[]>
+	results: ItemType[];
+	page: number;
+	carousel: ItemType[];
+	filters: Map<string, string[]>
 };
 
 export async function fetchProducts(
-  page: number,
-  category: "men" | "women" | "kids" | "technology"
+	page: number,
+	category: "men" | "women" | "kids" | "technology",
+	searchParams?: Record<string, string>
 ): Promise<ReturnType> {
-  let data;
+	let data;
 
-  switch (category) {
-    case "men":
-      data = (await import("../api/menProducts.json")).default;
-      break;
-    case "women":
-      data = (await import("../api/womenProducts.json")).default;
-      break;
-    case "kids":
-      data = (await import("../api/kidsProducts.json")).default;
-      break;
-    case "technology":
-      data = (await import("../api/technologyProducts.json")).default;
-      break;
-    default:
-      throw new Error(`Unknown category: ${category}`);
-  }
+	switch (category) {
+		case "men":
+			data = (await import("../api/menProducts.json")).default;
+			break;
+		case "women":
+			data = (await import("../api/womenProducts.json")).default;
+			break;
+		case "kids":
+			data = (await import("../api/kidsProducts.json")).default;
+			break;
+		case "technology":
+			data = (await import("../api/technologyProducts.json")).default;
+			break;
+		default:
+			throw new Error(`Unknown category: ${category}`);
+	}
 
-  const itemsPerPage = 20;
-  const skip = page * itemsPerPage;
-  const paginatedResults = data.slice(skip, skip + itemsPerPage) as ItemType[];
-  const carousel = data.length >= 12 ? data.slice(0, 12) as ItemType[] : data.length >= 6 ? data.slice(0, 6) as ItemType[] : [];
+	const itemsPerPage = 20;
+	const skip = page * itemsPerPage;
+	let paginatedResults = data.slice(skip, skip + itemsPerPage) as ItemType[];
+	const carousel = data.length >= 12 ? data.slice(0, 12) as ItemType[] : data.length >= 6 ? data.slice(0, 6) as ItemType[] : [];
 
-  const filters = paginatedResults.reduce((prev: Map<string, string[]>, curr) => {
-    const key = curr.optionsSelector.name;
+	console.log(searchParams)
+	if (searchParams && Object.keys(searchParams).length) {
+		const filteredResults = [] as ItemType[];
+		Object.entries(searchParams).forEach(([key, value]) => {
+			let filterName = paginatedResults.find(x => x.optionsSelector.name.toLocaleLowerCase() === key);
+			if (filterName !== undefined) {
+				for (let i = 0; i < paginatedResults.length; i++) {
+					if (paginatedResults[i].optionsSelector.name.toLocaleLowerCase() === key &&
+						paginatedResults[i].optionsSelector.options.map(x => x.toLocaleLowerCase()).includes(value)) {
+						filteredResults.push(paginatedResults[i])
+					}
+				}
+			}
+		})
+		console.log(filteredResults)
+		paginatedResults = filteredResults
+	}
 
-    if (prev.has(key)) {
-      const existingOptions = prev.get(key) || [];
-      const newOptions = [...existingOptions, ...curr.optionsSelector.options];
-      const uniqueOptions = Array.from(new Set(newOptions));
-      prev.set(key, uniqueOptions);
-    } else {
-      prev.set(key, curr.optionsSelector.options);
-    }
+	const filters = paginatedResults.reduce((prev: Map<string, string[]>, curr) => {
+		const key = curr.optionsSelector.name;
 
-    return prev;
-  }, new Map<string, string[]>());
+		if (prev.has(key)) {
+			const existingOptions = prev.get(key) || [];
+			const newOptions = [...existingOptions, ...curr.optionsSelector.options];
+			const uniqueOptions = Array.from(new Set(newOptions));
+			prev.set(key, uniqueOptions);
+		} else {
+			prev.set(key, curr.optionsSelector.options);
+		}
 
-  return {
-    results: paginatedResults,
-    page,
-    carousel,
-    filters
-  };
+		return prev;
+	}, new Map<string, string[]>());
+
+	return {
+		results: paginatedResults,
+		page,
+		carousel,
+		filters
+	};
 }
